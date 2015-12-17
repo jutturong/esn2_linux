0364f92d1d280d3c324632f0e30c581f17466f0d
jutturong <jutturongcharoenrit@gmail.com>
2015-12-17 20:17:03 +0700

    17-12-58 file2

--- js_reportPDF.php
+++ js_reportPDF.php
@@ -1,9 +1,26 @@
 <!--  31-12-2558  -->
 
 
-<script type="text/javascript">
-
-</script>
+    <script type="text/javascript">
+        function myformatter(date){
+            var y = date.getFullYear();
+            var m = date.getMonth()+1;
+            var d = date.getDate();
+            return y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d);
+        }
+        function myparser(s){
+            if (!s) return new Date();
+            var ss = (s.split('-'));
+            var y = parseInt(ss[0],10);
+            var m = parseInt(ss[1],10);
+            var d = parseInt(ss[2],10);
+            if (!isNaN(y) && !isNaN(m) && !isNaN(d)){
+                return new Date(y,m-1,d);
+            } else {
+                return new Date();
+            }
+        }
+    </script>
     
     
 <div id="win_report" class="easyui-window" title=" Report ESN System ( ออกผลรายงาน ) " 
